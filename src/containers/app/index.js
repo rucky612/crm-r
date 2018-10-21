91158feca521154200b83469ac3237878bfabbad
rucky612 <rucky612@gmail.com>
2018-10-21 18:54:11 +0900

    Revert "todo"
    This reverts commit 6ee16202a144cf00465f1e3df17e32d6bed040fe.

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,21 +1,9 @@
 import React from 'react'
-import { Route } from 'react-router'
-
-import Header from '../layouts/Header/index'
-import Aside from '../layouts/Aside/index'
-import Templates from '../pages/Templates/index'
-import Messages from '../pages/Messages/index'
-import Utils from '../pages/Utils/index'
+import { Route, Link } from 'react-router-dom'
 
 const App = () => (
-  <div className={"sgsg-layout"}>
-    <Header/>
-    <Aside/>
-    <main className={`sgsg-pages`}>
-      <Route path="/templates" component={Templates} />
-      <Route path="/messages" component={Messages} />
-      <Route path="/utils" component={Utils} />
-    </main>
+  <div>
+    hello
   </div>
 )
 
